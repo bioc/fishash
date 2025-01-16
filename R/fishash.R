20e77c2532efc48c2f5188cd8206db4e0598caa1
Jack Kamm <jackkamm@gmail.com>
2025-01-16 11:13:31 -0800

    Add GS FDR method

--- R/fishash.R
+++ R/fishash.R
@@ -2,25 +2,25 @@
 #'
 #' @param counts Matrix of counts. Typically, columns are cells and
 #'   rows are features (e.g. gRNAs).
-#' @param fdr_cutoff Threshold for false discovery rate
-#' @param fdr_method FDR correction type -- either "BH" for
-#'   Benjamini-Hochberg or "BY" for Benjamini-Yekutueli
+#' @param padj_cutoff Threshold for false discovery rate
+#' @param padj_method FDR correction type. "BH" for Benjamini-Hochberg
+#'   "BY" for Benjamini-Yekutueli, "GS" for Guo & Sarkar 2020 assuming
+#'   independence across cells (and arbitrary dependence within).
 #'
 #' @returns A SummarizedExperiment, containing assays for the
-#'   assignment ("assigned"), log-p-value ("log_pval"), 1 minus
-#'   BH-corrected FDR ("padj_bh_1m"), 1 minus BY-corrected FDR
-#'   ("padj_by_1m"). The colData has columns for whether the cell is
-#'   singlet, doublet, or unassigned ("demux_type"), and the
-#'   assignment ("assignment"). The metadata contains an entry for the
-#'   p-value cutoff at the given FDR level ("log_pval_cutoff"), as
-#'   well as cutoffs under both BH and BY ("log_pval_cutoff_bh",
-#'   "log_pval_cutoff_by").
+#'   assignment matrix ("assigned"), and log-p-value ("log_pval"). The
+#'   colData has columns for whether the cell is singlet, doublet, or
+#'   unassigned ("demux_type"), and the assignment as a
+#'   comma-delimited string ("assignment"). The metadata contains an
+#'   entry for the p-value cutoff at the given FDR level
+#'   ("log_pval_cutoff").
 #'
 #' @export
 #' @importFrom SummarizedExperiment SummarizedExperiment
 #' @importFrom Matrix colSums rowSums
-fishash <- function(counts, fdr_cutoff=.05, fdr_method=c("Holm", "BY", "BH")) {
-  fdr_method <- match.arg(fdr_method)
+#' @importFrom sparseMatrixStats colMins
+fishash <- function(counts, padj_cutoff=.05, padj_method=c("GS", "BY", "BH")) {
+  padj_method <- match.arg(padj_method)
 
   tot <- sum(counts)
 
@@ -28,8 +28,6 @@ fishash <- function(counts, fdr_cutoff=.05, fdr_method=c("Holm", "BY", "BH")) {
   row_sums <- rowSums(counts)
 
   n_entries <- as.numeric(nrow(counts)) * as.numeric(ncol(counts))
-
-  cm <- log(n_entries) + 1/(2*n_entries) + .57721
 
   counts <- as(counts, 'CsparseMatrix')
   counts <- as(counts, 'TsparseMatrix')
@@ -52,53 +50,42 @@ fishash <- function(counts, fdr_cutoff=.05, fdr_method=c("Holm", "BY", "BH")) {
     log.p=TRUE
   )
 
-  df <- df[order(df$log_pval),]
-  df$rank <- 1:nrow(df)
-  df$padj_by <- pmin(n_entries * cm * exp(df$log_pval) / df$rank, 1)
-  df$padj_bh <- pmin(n_entries * 1 * exp(df$log_pval) / df$rank, 1)
-  df$padj_holm <- exp(df$log_pval) * (n_entries - df$rank + 1)
-  df$padj_holm <- pmin(cummax(df$padj_holm), 1)
-
-  df <- df[nrow(df):1,]
-  df$padj_by <- cummin(df$padj_by)
-  df$padj_bh <- cummin(df$padj_bh)
-
-  df$padj_bh_1m <- 1-df$padj_bh
-  df$padj_by_1m <- 1-df$padj_by
-  df$padj_holm_1m <- 1-df$padj_holm
-
-  # TODO add assigned, odds_ratio
-  assay_names <- c('log_pval', 'padj_by_1m', 'padj_bh_1m', 'padj_holm_1m')
-  names(assay_names) <- assay_names
-
-  ret <- lapply(
-    assay_names,
-    function(x) {
-      sparseMatrix(
-        i=df$row_idx, j=df$col_idx,
-        x=df[,x], dims=dim(counts), dimnames=dimnames(counts),
-        index1=TRUE
-      )
-    }
+  mat_logpval <- sparseMatrix(
+    i=df$row_idx, j=df$col_idx,
+    x=df$log_pval, dims=dim(counts), dimnames=dimnames(counts),
+    index1=TRUE
   )
 
-  ret <- SummarizedExperiment(ret)
-
-  metadata(ret)$log_pval_cutoff_by <- max(df[df$padj_by <= fdr_cutoff,'log_pval'])
-  metadata(ret)$log_pval_cutoff_bh <- max(df[df$padj_bh <= fdr_cutoff,'log_pval'])
-  metadata(ret)$log_pval_cutoff_holm <- max(df[df$padj_holm <= fdr_cutoff,'log_pval'])
-
-  if (fdr_method == 'Holm') {
-    metadata(ret)$log_pval_cutoff <- metadata(ret)$log_pval_cutoff_holm
-  } else if (fdr_method == 'BH') {
-    metadata(ret)$log_pval_cutoff <- metadata(ret)$log_pval_cutoff_bh
-  } else if (fdr_method == 'BY') {
-    metadata(ret)$log_pval_cutoff <- metadata(ret)$log_pval_cutoff_by
+  if (padj_method == "BY") {
+    cm <- log(n_entries) + 1/(2*n_entries) + .57721
   } else {
-    stop(sprintf("Unrecognized FDR method %s", fdr_method))
+    cm <- 1
   }
 
-  # TODO add colData
+  if (padj_method %in% c("BH", "BY")) {
+    df <- df[order(df$log_pval),]
+    df$rank <- 1:nrow(df)
 
-  ret
+    df$padj <- pmin(n_entries * cm * exp(df$log_pval) / df$rank, 1)
+
+    df <- df[nrow(df):1,]
+    df$padj <- cummin(df$padj)
+
+    n_signif <- sum(df$padj <= padj_cutoff)
+    logpval_cutoff <- log(padj_cutoff) - log(cm) - log(n_entries) + log(n_signif)
+    stopifnot(sum(df$log_pval <= logpval_cutoff) == n_signif)
+  } else if (padj_method == "GS") {
+    colmin_logpval <- colMins(mat_logpval)
+    block_padj <- p.adjust(exp(colmin_logpval) * nrow(counts), method='BH')
+    B <- sum(block_padj <= padj_cutoff)
+
+    logpval_cutoff <- log(padj_cutoff) - log(n_entries) + log(B)
+  }
+
+  mat_assigned <- mat_logpval <= logpval_cutoff
+
+  SummarizedExperiment(
+    assays=list(assigned=mat_assigned, log_pval=mat_logpval),
+    metadata=list(log_pval_cutoff=logpval_cutoff)
+  )
 }
