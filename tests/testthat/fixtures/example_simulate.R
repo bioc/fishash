87939c1fb588caba622508e733e2b767f20235c6
Jack Kamm <jackkamm@gmail.com>
2026-07-18 17:12:47 -0700

    Minor additional linting

--- tests/testthat/fixtures/example_simulate.R
+++ tests/testthat/fixtures/example_simulate.R
@@ -1,17 +1,16 @@
-
 devtools::load_all()
 set.seed(12345)
 
 res <- simulate_guidebender2(
-    n_guides=1000,
-    n_cells=1000,
-    moi=.3,
-    hurdle_prob=.1,
-    snr=4,
-    count_per_cell=100,
-    frac_noise_endo=.75,
+    n_guides = 1000,
+    n_cells = 1000,
+    moi = .3,
+    hurdle_prob = .1,
+    snr = 4,
+    count_per_cell = 100,
+    frac_noise_endo = .75,
     #chunk_cells=100,
-    return_sparse_only=TRUE
+    return_sparse_only = TRUE
 )
 
 saveRDS(res, "tests/testthat/fixtures/example_simulate.rds")
